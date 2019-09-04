e37d19c19686f738f862465aba1eacd548bb4fe5
Hayk Mkrtchyan <hayk.mkrtchyan@sflpro.com>
2019-09-04 21:21:55 +0400

    Updated the readme file.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # demo
+
+
+Just pass db
+https://cloud.docker.com/u/haykm/repository/docker/haykm/demo-notes-api
+
+https://cloud.docker.com/u/haykm/repository/docker/haykm/demo-web-gateway
--- web-gateway/src/main/java/io/hayk/demo/resources/note/user/UserController.java
+++ web-gateway/src/main/java/io/hayk/demo/resources/note/user/UserController.java
@@ -5,10 +5,7 @@ import io.hayk.demo.note.user.BindExternalAccountRequest;
 import io.hayk.demo.note.user.BindExternalAccountResponse;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.security.Principal;
