e23fdb99aa9a89011a0ec64f7c31544b25815aef
Hayk Mkrtchyan <hayk.m.mkrtchyan@gmail.com>
2021-03-29 13:52:24 +0400

    Updating junit version.

---
+++ user/api-model/src/main/java/io/hayk/rsocketdemo/api/model/GetUserIdByExternalAccountUidResult.java
@@ -0,0 +1,39 @@
+package io.hayk.rsocketdemo.api.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GetUserIdByExternalAccountUidResult {
+
+    private Long userId;
+
+    private List<String> errors;
+
+    public GetUserIdByExternalAccountUidResult() {
+        super();
+    }
+
+    public GetUserIdByExternalAccountUidResult(final Long userId) {
+        this.userId = userId;
+    }
+
+    public GetUserIdByExternalAccountUidResult(final List<String> errors) {
+        this.errors = errors;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
+    }
+}
